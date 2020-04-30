60b86bfe038b8d6b90dc41d2be139763117df3f5
Amir Sekhavati <amir.sekhavati@gmail.com>
2020-04-30 14:32:14 +0100

    layout transactions section

--- lib/widgets/transactions.dart
+++ lib/widgets/transactions.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutterbank/constants/colors.dart';
 import 'package:flutterbank/widgets/transaction/transaction.dart';
 
 class Transactions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 5,
-      itemBuilder: (BuildContext context, int index) {
-        return Padding(
-          padding: const EdgeInsets.only(
-            bottom: 24,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Transactions',
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            color: kSwatchColor2,
           ),
-          child: Transaction(),
-        );
-      },
+        ),
+        SizedBox(
+          height: 28,
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 24,
+                ),
+                child: Transaction(),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
