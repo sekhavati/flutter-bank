4687c559dcc74208dbbc157ad1c6e3834abcfb8a
Amir Sekhavati <amir.sekhavati@gmail.com>
2020-05-04 14:52:46 +0100

    styling tweaks to HistoricalBalance widget

--- lib/widgets/historical_balance.dart
+++ lib/widgets/historical_balance.dart
@@ -1,81 +1,92 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutterbank/constants/colors.dart';
 
 class HistoricalBalance extends StatelessWidget {
-  List<Color> gradientColors = [
-    kSwatchColor9,
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return LineChart(
-      LineChartData(
-        backgroundColor: kSwatchColor2,
-        maxY: 10,
-        borderData: FlBorderData(
-          show: false,
-        ),
-        gridData: FlGridData(
-          show: true,
-          drawVerticalLine: true,
-          drawHorizontalLine: false,
-          getDrawingVerticalLine: (value) {
-            return FlLine(
-              color: const Color(0xff37434d),
-              strokeWidth: 1,
-            );
-          },
-        ),
-        titlesData: FlTitlesData(
-          bottomTitles: SideTitles(
-            showTitles: true,
-            getTitles: (month) {
-              switch (month.toInt()) {
-                case 0:
-                  return "JAN";
-                case 1:
-                  return "FEB";
-                case 2:
-                  return "MAR";
-                case 3:
-                  return "APR";
-                case 4:
-                  return "MAY";
-                case 5:
-                  return "JUN";
-                default:
-                  return "TODO";
-              }
-            },
-          ),
-          leftTitles: SideTitles(showTitles: false),
-        ),
-        lineBarsData: [
-          LineChartBarData(
-            colors: [kSwatchColor9],
-            dotData: FlDotData(
-              show: false,
+    return AspectRatio(
+      aspectRatio: 16.0 / 9.0,
+      child: Container(
+        color: kSwatchColor2,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: LineChart(
+            LineChartData(
+              backgroundColor: kSwatchColor2,
+              maxY: 7,
+              borderData: FlBorderData(
+                show: false,
+              ),
+              gridData: FlGridData(
+                show: true,
+                drawVerticalLine: true,
+                drawHorizontalLine: false,
+                getDrawingVerticalLine: (value) {
+                  return FlLine(
+                    color: const Color(0xff37434d),
+                    strokeWidth: 1,
+                  );
+                },
+              ),
+              titlesData: FlTitlesData(
+                bottomTitles: SideTitles(
+                  showTitles: true,
+                  textStyle: const TextStyle(
+                    color: kSwatchColor4,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
+                  getTitles: (month) {
+                    switch (month.toInt()) {
+                      case 0:
+                        return "Jan";
+                      case 1:
+                        return "Feb";
+                      case 2:
+                        return "Mar";
+                      case 3:
+                        return "Apr";
+                      case 4:
+                        return "May";
+                      case 5:
+                        return "Jun";
+                      default:
+                        return '';
+                    }
+                  },
+                ),
+                leftTitles: SideTitles(showTitles: false),
+              ),
+              lineBarsData: [
+                LineChartBarData(
+                  colors: [kSwatchColor9],
+                  dotData: FlDotData(
+                    show: false,
+                  ),
+                  isCurved: true,
+                  preventCurveOverShooting: true,
+                  barWidth: 4,
+                  belowBarData: BarAreaData(
+                    show: true,
+                    colors: [
+                      kSwatchColor9.withOpacity(0.3),
+                    ],
+                  ),
+                  spots: [
+                    FlSpot(0, 1),
+                    FlSpot(1, 3),
+                    FlSpot(2, 2),
+                    FlSpot(3, 4),
+                    FlSpot(4, 3),
+                    FlSpot(5, 5),
+                  ],
+                ),
+              ],
             ),
-            isCurved: true,
-            preventCurveOverShooting: true,
-            barWidth: 4,
-            belowBarData: BarAreaData(
-              show: true,
-              colors: gradientColors
-                  .map((color) => color.withOpacity(0.3))
-                  .toList(),
-            ),
-            spots: [
-              FlSpot(0, 3),
-              FlSpot(1, 1),
-              FlSpot(2, 5),
-              FlSpot(3, 6),
-              FlSpot(4, 0),
-              FlSpot(5, 2),
-            ],
           ),
-        ],
+        ),
       ),
     );
   }
